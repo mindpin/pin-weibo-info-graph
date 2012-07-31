ff65848707877aa86da8b0bc7a55e3c2d0987184
Xiao Rich <xiaorich@box.(none)>
2012-07-31 14:32:39 +0800

    Fix weibo_status of comment

--- app/models/weibo_comment.rb
+++ app/models/weibo_comment.rb
@@ -1,7 +1,7 @@
 class WeiboComment < ActiveRecord::Base
   belongs_to :weibo_status, 
              :class_name => 'WeiboStatus', 
-             :foreign_key => :weibo_status_id
+             :foreign_key => :weibo_status_id, :primary_key => :weibo_status_id
   
   validates_uniqueness_of :weibo_comment_id
 end
